6d11b173b71ed059e6145fe2c28693f1c493b614
Orlando <orlandoortegajr@gmail.com>
2019-03-09 14:33:15 -0500

    Added different screens for each menu item, and also enabled action handling

--- app/src/main/java/com/example/safeparkingzonesapp/MainActivity.java
+++ app/src/main/java/com/example/safeparkingzonesapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.safeparkingzonesapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -20,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Assing drawer_layout
+        //Assign drawer_layout
         drawer = findViewById(R.id.drawer_layout);
+
+        //Allows for listening to click events
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         //Makes hamburger menu button rotate on open and close
@@ -29,6 +37,58 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if (savedInstanceState == null) {
+
+            //Syncs the Near and Safe Fragment as the starter screen
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new NearAndSafeFragment()).commit();
+
+        }
+
+        navigationView.setCheckedItem(R.id.nav_near_and_safe);
+
+
+    }
+
+    //Allows for action to occur when item is selected, also denotes what happens when each menu item is clicked
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_near_and_safe:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NearAndSafeFragment()).commit();
+                break;
+
+            case R.id.nav_near:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NearFragment()).commit();
+                break;
+
+            case R.id.nav_safe:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SafeFragment()).commit();
+                break;
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     //handles correct opening and closing of our menu
