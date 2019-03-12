f1932034240feb935201ddbc698185a100548721
Sharma Bilaval <sharmb9@mcmaster.ca>
2019-03-12 18:21:45 -0400

    Added sort by distance button and a list view in second acivity

--- app/src/main/java/com/myappcompany/user/safeparkingzones/SortedByDistActivity.java
+++ app/src/main/java/com/myappcompany/user/safeparkingzones/SortedByDistActivity.java
@@ -2,6 +2,7 @@ package com.myappcompany.user.safeparkingzones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -19,31 +20,40 @@ public class SortedByDistActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sorted_by_dist);
 
         //add code to sort the parking spots by distance
-        ListView sortedListView= findViewById(R.id.sortedListView);
+        ListView sortedListView = findViewById(R.id.sortedListView);
+        ArrayList<String> sortedSpots = new ArrayList<String>();
+        //REMOVE THIS LATER
+        sortedSpots.add("Spot A");
+        sortedSpots.add("Spot B");
+        //adds sorted parking spots to an arraylist (from dataset for now(unsorted))
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("final_parking_zones.csv")))) {
+//            //an arrayList of sorted coordinates (replace this with sorted coordinates)
+//
+//            String line = "";
+//            //skips first line
+//            try {
+//                reader.readLine();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            int count = 0;
+//            try {
+//                while (count <= 15) // Reads first 15 coordinates to load data quickly, change this later to search coordinates within 500 m os searches location
+//                {
+//                    line = reader.readLine();
+//                    //adds sorted parking spots to array list
+//                    sortedSpots.add("Spot A");
+//                    count++;
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
-        //adding parking spots from dataset for now(unsorted)
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("final_parking_zones.csv")))) {
-            //an arrayList of sorted coordinates (replace this with sorted coordinates)
-            ArrayList<String> sortedSpots = new ArrayList<String>();
-            String line = "";
-            //skips first line
-            try {
-                reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        //put the data addresses from the arraylist in listView
 
-            int count = 0;
-            try {
-                while (count <= 15) // Reads first 15 coordinates to load data quickly, change this later to search coordinates within 500 m os searches location
-                {
-                    line = reader.readLine();
-                    //adds sorted parking spots to array list
-                    sortedSpots.add(line);
-                    count++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sortedSpots);
+        sortedListView.setAdapter(arrayAdapter);
+    }
 }
