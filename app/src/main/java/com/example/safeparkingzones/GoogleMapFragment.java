532973516cf966ac42b54c6b0a07f6e8ab87a8f8
Orlando <orlandoortegajr@gmail.com>
2019-03-12 00:35:38 -0400

    Added Bilaval's searchbar to the main screen, need to fix search bar functionality

--- app/src/main/java/com/example/safeparkingzones/GoogleMapFragment.java
+++ app/src/main/java/com/example/safeparkingzones/GoogleMapFragment.java
@@ -1,5 +1,8 @@
 package com.example.safeparkingzones;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,13 +10,22 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -34,6 +46,122 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
+    //Loads markers from dataset to google maps
+    public void loadMarkers(String dataset){
+        List<LatLng> latLngList;
+//        change the file to parking spots because that is what we are supposed to show with markers
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.getContext().getAssets().open(dataset)))) {
+            //an arrayList of type LatLng(40,95)
+            latLngList = new ArrayList<LatLng>();
+            String line = "";
+//        skips first line
+            try {
+                reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int count = 0;
+            try {
+                while (count <= 15) // Reads first 15 coordinates to load data quickly, change this later to search coordinates within 500 m os searches location
+                {
+                    line = reader.readLine();
+                    //converts address to coordinates
+                    LatLng address = getLocationFromAddress(this.getContext(), line);
+                    //adds to convereted coordinates to arrayList
+                    latLngList.add(new LatLng(address.latitude, address.longitude));
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int coordCount=0;
+            for(LatLng pos : latLngList)
+            {
+//                Log.i("Position", String.valueOf(pos));
+                LatLng parkingSpot = new LatLng(pos.latitude,pos.longitude);
+                //adding different color markers to different coordinates, will change this later where colors will be assigned according to safety level
+                if(coordCount<=5){
+                    mMap.addMarker(new MarkerOptions()
+                            .position(parkingSpot)
+                            .title("Parking Spot")// change title to something more descriptive
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                }
+                else if (coordCount>5 && coordCount <=10){
+                    mMap.addMarker(new MarkerOptions()
+                            .position(parkingSpot)
+                            .title("Parking Spot")// change title to something more descriptive
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                }
+                else{
+                    mMap.addMarker(new MarkerOptions()
+                            .position(parkingSpot)
+                            .title("Parking Spot")// change title to something more descriptive
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                }
+
+                coordCount++;
+            }
+            //ZOOMS camera to first location from dataset for now, will change location later
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngList.get(1),12));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onMapSearch(View view){
+        EditText locationSearch = (EditText) getView().findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this.getContext());
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address searchedAddress = addressList.get(0);
+            LatLng latLng = new LatLng(searchedAddress.getLatitude(), searchedAddress.getLongitude());
+            //adds marker to searched location(will change color of this later to make it distinct from parking spots
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Location"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            //loads parkings spots
+            loadMarkers("final_parking_zones.csv");
+        }
+    }
+
+    //Use GeoCoding to convert Parking Spot addresses to coordinates
+    public LatLng getLocationFromAddress(Context context, String strAddress)
+    {
+        Geocoder coder= new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try
+        {
+            address = coder.getFromLocationName(strAddress, 5);
+            if(address==null)
+            {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return p1;
+
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -45,11 +173,13 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //show a marker here too
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(41.8781, -87.62938);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Chicago"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
+
